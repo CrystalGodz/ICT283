105e38edc2f44bf3004dfa6ccf58924638df1468
CrystalGodz <keigolim81@gmail.com>
2026-03-18 11:09:19 +0800

    Add files via upload

---
+++ Labs/Practical-Lab4/ICT283_Lab4Pt2/Light.h
@@ -0,0 +1,30 @@
+#ifndef LIGHT_H_INCLUDED
+#define LIGHT_H_INCLUDED
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Light{
+public:
+    Light();
+    ~Light() {}     //destructor has no implementation now
+
+    //void GetColour(string& colour) const;   correct way for objects
+    string GetColour() const;               //allowable if string is small
+    float GetRadius() const;
+    bool IsOn() const;
+
+    void SetColour(const string& colour);
+    void SetRadius(const float& radius);
+
+    friend ostream& operator << (ostream& ostr, const Light& light);
+
+private:
+    string m_colour;
+    float m_radius;
+    bool m_on;
+};
+
+
+#endif // LIGHT_H_INCLUDED
